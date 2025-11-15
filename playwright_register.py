0df87bd515ff2509808546f3aa0484bd361ab31a
Max <maks.krutikov.2002@gmail.com>
2025-11-15 20:23:29 +0400

    Интеграция Pytest и Playwright

--- playwright_register.py
+++ playwright_register.py
@@ -29,4 +29,3 @@ with sync_playwright() as playwright:
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
 
-    page.wait_for_timeout(5000)
---
+++ tests/test_pytest.py
@@ -0,0 +1,21 @@
+
+
+
+def test_login_user():
+    print("Hello")
+
+
+class TestUserLogin:
+    def test_1(self):
+        pass
+
+    def test_2(self):
+        pass
+
+
+def test_assert_positive():
+    assert 2 + 2 == 4
+
+
+def test_assert_negative():
+    assert 2 + 2 == 5
