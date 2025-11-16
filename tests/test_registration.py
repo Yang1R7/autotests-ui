a2a552bbd9012d995f336aab7bb33d15f996c0a2
Max <maks.krutikov.2002@gmail.com>
2025-11-16 19:09:43 +0400

    Практика работы с Page Object

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,24 +1,14 @@
+import time
+
 from playwright.sync_api import sync_playwright, expect
 import pytest
 
 
 @pytest.mark.regression
 @pytest.mark.registration
-def test_successful_registration(chromium_page):
-        chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+def test_successful_registration(registration_page, dashboard_page):
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form(email="user.name@gmail.com", username="username", password="password")
+    registration_page.click_registration_button()
 
-        email_input = chromium_page.get_by_test_id("registration-form-email-input").locator("input")
-        email_input.fill("user.name@gmail.com")
-
-        username_input = chromium_page.get_by_test_id("registration-form-username-input").locator("input")
-        username_input.fill("username")
-
-        password_input = chromium_page.get_by_test_id("registration-form-password-input").locator("input")
-        password_input.fill("password")
-
-        registration_button = chromium_page.get_by_test_id("registration-page-registration-button")
-        registration_button.click()
-
-        dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-        expect(dashboard_title).to_be_visible()
-
+    dashboard_page.check_dashboard_title()
