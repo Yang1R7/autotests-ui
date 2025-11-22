353b5b5ae354d24f0cccd9ef49bbec3029beb9c9
Max <maks.krutikov.2002@gmail.com>
2025-11-23 01:57:04 +0400

    Практическая работа с Allure аннотациями

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -1,12 +1,28 @@
+import allure
 import pytest
 from pages.authentication.login_page import LoginPage
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.epics import AllureEpic
+from tools.allure.feature import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
+from allure_commons.types import Severity
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag("REGRESSION", "AUTHORIZATION")
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.AUTHORIZATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.AUTHORIZATION)
 class TestAuthentication:
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.title('User login with correct email and password')
+    @allure.severity(Severity.BLOCKER)
     def test_successful_authorization(
             self,
             registration_page: RegistrationPage,
@@ -33,6 +49,9 @@ class TestAuthentication:
         ("user.name@gmail.com", "  "),
         ("  ", "password")
     ])
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.title('User login with wrong email or password')
+    @allure.severity(Severity.CRITICAL)
     def test_wrong_email_or_password_authorization(self, login_page: LoginPage, email: str, password: str):
         login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
         login_page.login_form.fill(email=email, password=password)
@@ -40,6 +59,9 @@ class TestAuthentication:
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
 
+    @allure.tag(AllureTag.NAVIGATION)
+    @allure.title("Navigation from login page to registration page")
+    @allure.severity(Severity.NORMAL)
     def test_navigate_from_authorization_to_registration(
             self,
             login_page: LoginPage,
@@ -49,5 +71,3 @@ class TestAuthentication:
         login_page.click_registration_link()
 
         registration_page.register_form.check_visible(email="", username="", password="")
-
-
---
+++ tools/allure/epics.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+
+
+class AllureEpic(str, Enum):
+    LMS = "LMS system"
+    STUDENT = "Student system"
+    ADMINISTRATION = "Administration system"
